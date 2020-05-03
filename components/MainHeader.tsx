00a42b9f28c3e36f6e869299cbd3a57d648c5a3c
李铭辉 <1018596741@qq.com>
2020-05-03 10:08:12 +0800

    完成响应式头部菜单按钮

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,4 +1,4 @@
-import React,{useRef} from 'react';
+import React,{useRef, useState, useCallback} from 'react';
 
 export interface ComponentProps {
 
@@ -6,11 +6,24 @@ export interface ComponentProps {
 
 const MainHeader: React.FC<ComponentProps> = () => {
     const headerRef = useRef<HTMLDivElement>(null);
+    const [btnClass, setBtnClass] = useState<string>('');
+    const [menuVisible, setMenuVisible] = useState<boolean>(false)
+    const clickBtn = useCallback(()=>{
+        setMenuVisible(!menuVisible);
+        console.log(menuVisible)
+        if(menuVisible === true){
+            setBtnClass('menu-btn-active')
+        }else{
+            setBtnClass('')
+        }
+    },[menuVisible])
     return (
         <div ref={headerRef} className="header-wrapper">
             <div className="header-menu">
-                <section className="header-menu-logo">logo</section>
-                <section className="header-menu-btn">三</section>
+                <section className="header-menu-logo">CLee's Blog</section>
+                <section className="header-menu-btn">
+                    <img onTouchEnd={clickBtn} className={btnClass} src={"../static/menu.svg"}alt=""/>
+                </section>
                 <ul className="header-menu-content">
                     <li className="header-menu-content-item">首页</li>
                     <li className="header-menu-content-item">分类</li>

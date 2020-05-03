acaad1dfa42d07a37a03d805f876ffff1a9082a7
李铭辉 <1018596741@qq.com>
2020-05-03 22:40:58 +0800

    完成顶部栏

--- components/MainHeader.tsx
+++ components/MainHeader.tsx
@@ -1,4 +1,4 @@
-import React,{useRef, useState, useCallback} from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 
 export interface ComponentProps {
 
@@ -6,31 +6,39 @@ export interface ComponentProps {
 
 const MainHeader: React.FC<ComponentProps> = () => {
     const headerRef = useRef<HTMLDivElement>(null);
-    const [btnClass, setBtnClass] = useState<string>('');
-    const [menuVisible, setMenuVisible] = useState<boolean>(false)
-    const clickBtn = useCallback(()=>{
+    const [btnClass, setBtnClass] = useState<string>('header-menu-btn');
+    const [menuVisible, setMenuVisible] = useState<boolean>(false);
+    const [menuBottom, setMenuBottom] = useState<string>('32vh');
+    const clickBtn = useCallback(() => {
         setMenuVisible(!menuVisible);
-        console.log(menuVisible)
-        if(menuVisible === true){
-            setBtnClass('menu-btn-active')
-        }else{
-            setBtnClass('')
+        if (menuVisible === true) {
+            setBtnClass('header-menu-btn-active');
+            setMenuBottom('-20vh');
+        } else {
+            setBtnClass('header-menu-btn');
+            setMenuBottom('32vh')
         }
-    },[menuVisible])
+    }, [menuVisible])
     return (
         <div ref={headerRef} className="header-wrapper">
             <div className="header-menu">
                 <section className="header-menu-logo">CLee's Blog</section>
-                <section className="header-menu-btn">
-                    <img onTouchEnd={clickBtn} className={btnClass} src={"../static/menu.svg"}alt=""/>
+                <section className={btnClass}>
+                    <img onTouchEnd={clickBtn} src={"../static/menu.svg"} alt="" />
                 </section>
-                <ul className="header-menu-content">
+                <ul className="header-menu-content" >
                     <li className="header-menu-content-item">首页</li>
                     <li className="header-menu-content-item">分类</li>
                     <li className="header-menu-content-item">项目</li>
                     <li className="header-menu-content-item">关于我</li>
                 </ul>
             </div>
+            <ul className="header-menu-flexContent" style={{ bottom: menuBottom }}>
+                <li className="header-menu-flexContent-item">首页</li>
+                <li className="header-menu-flexContent-item">分类</li>
+                <li className="header-menu-flexContent-item">项目</li>
+                <li className="header-menu-flexContent-item">关于我</li>
+            </ul>
         </div>
     )
 }

d8bdd663f503326008cf02d2481736774b1e187e
李铭辉 <1018596741@qq.com>
2020-05-12 23:37:15 +0800

    分类页布局

--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -9,8 +9,14 @@ const FComponent: React.FC<ComponentProps> = (props: ComponentProps) => {
     console.log(props)
     return (
         <Layout>
-            <div className="category-wrapper">
-                分类页:1
+            <div className="main-wrapper">
+                <div className="main-body">
+                    <div className="category-content">
+                        <section className="category-content-cover">封面</section>
+                        <section className="category-content-tags">标签</section>
+                        <section className="category-content-list">列表</section>
+                    </div>
+                </div>
             </div>
         </Layout>
     )

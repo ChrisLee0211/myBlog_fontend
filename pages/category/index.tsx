47359fd99bb121ebd2e39bd9f4dbaae5fbbf77f3
李铭辉 <1018596741@qq.com>
2020-05-13 23:42:03 +0800

    完成分类页上半部分

--- pages/category/index.tsx
+++ pages/category/index.tsx
@@ -12,8 +12,20 @@ const FComponent: React.FC<ComponentProps> = (props: ComponentProps) => {
             <div className="main-wrapper">
                 <div className="main-body">
                     <div className="category-content">
-                        <section className="category-content-cover">封面</section>
-                        <section className="category-content-tags">标签</section>
+                        <section className="category-content-cover">
+                            <img src="https://user-gold-cdn.xitu.io/2020/5/8/171f3c48e142a180?imageView2/0/w/1280/h/960/format/webp/ignore-error/1" alt=""/>
+                        </section>
+                        <section className="category-content-tags">
+                            <span className="category-content-tags-title"><img src="../../static/tag.svg" alt=""/>标签</span>
+                            <ul className="category-content-tags-list">
+                                <li>标签一</li>
+                                <li>标签一</li>
+                                <li>标签一</li>
+                                <li>标签一</li>
+                                <li>标签一</li>
+                                <li>标签一</li>
+                            </ul>
+                        </section>
                         <section className="category-content-list">列表</section>
                     </div>
                 </div>

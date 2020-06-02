85d75d74964d96c054ed5478aef41a41266dc665
李铭辉 <1018596741@qq.com>
2020-06-02 23:53:48 +0800

    编写回到顶部按钮显示监听逻辑

--- pages/article/[id].tsx
+++ pages/article/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState, useRef  } from 'react';
 import ReactMarkdown from 'react-markdown'
 import {GetStaticProps,GetStaticPaths} from 'next';
 import Layout from '../../components/Layout';
@@ -32,17 +32,24 @@ export interface ComponentProps {
 
 const ArticleContent: React.FC<ComponentProps> = (props: ComponentProps) => {
     const { article }= props;
+    const [srollTopVisible,setScrollTopVisible] = useState<boolean>(false);
+    const wrapperRef = useRef<HTMLDivElement>(null);
     useEffect(()=>{
-        document.addEventListener("scroll",e => {
-            console.log(e)
-        });
-        return document.removeEventListener("scroll", e=> {
-            console.log(e)
-        })
+        console.log(wrapperRef.current)
+        function watchScroll(e:any){
+            const scrollTop = e.target.scrollingElement.scrollTop;
+            if(scrollTop > 10){
+                setScrollTopVisible(true)
+            }else{
+                setScrollTopVisible(false)
+            }
+        }
+        document.addEventListener("scroll",watchScroll);
+        // return document.removeEventListener("scroll", watchScroll)
     },[])
     return (
         <Layout>
-            <div className="main-wrapper">
+            <div className="main-wrapper" ref={wrapperRef}>
                 <div className="main-body">
                     <section className="article-header">
                         <div className="article-header-title">{article.title}</div>
@@ -51,8 +58,59 @@ const ArticleContent: React.FC<ComponentProps> = (props: ComponentProps) => {
                             <span className="article-header-baseInfo-date">创建时间：{parseTime(article.created_at)}</span>
                         </div>
                     </section>
-                    <section className="article-nav">
-                        {/* 导航栏 */}
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
+                    </section>
+                    <section className="article-nav" style={{padding:"200px 0px"}}>
+                        {/* 导航栏 */}xxxxxx
                     </section>
                     <section className="article-body">
                         <ReactMarkdown 
@@ -60,9 +118,10 @@ const ArticleContent: React.FC<ComponentProps> = (props: ComponentProps) => {
                             renderers={{code:CodeBlock}}
                             escapeHtml={false}/>
                     </section>
-                    <section className="article-scrollTop">
+                    <div 
+                        className={srollTopVisible?"article-scrollTop scrollTop-active":"article-scrollTop"}>
                         <img src="../../static/scrollTop.png" alt="" />
-                    </section>
+                    </div>
                 </div>
             </div>
         </Layout>
